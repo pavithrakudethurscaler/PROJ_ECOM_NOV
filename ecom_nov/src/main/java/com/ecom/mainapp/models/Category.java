8332a7f4c9539b85437c5aa9733a52474ec4bbd8
Pavithra Kudethur <pavithra.kudethur@gmail.com>
2025-12-18 11:50:12 +0530

    C5: Fetch Types & Fetch Modes

--- ecom_nov/src/main/java/com/ecom/mainapp/models/Category.java
+++ ecom_nov/src/main/java/com/ecom/mainapp/models/Category.java
@@ -1,8 +1,12 @@
 package com.ecom.mainapp.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Getter
 @Setter
--- ecom_nov/src/main/java/com/ecom/mainapp/models/Product.java
+++ ecom_nov/src/main/java/com/ecom/mainapp/models/Product.java
@@ -3,9 +3,12 @@ package com.ecom.mainapp.models;
 import com.ecom.mainapp.dtos.FakeStoreProductDto;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Getter
 @Setter
@@ -14,7 +17,8 @@ public class Product extends BaseModel {
     private String name;
     private String description;
     private double price;
-    @ManyToOne(cascade = CascadeType.DETACH)
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @Fetch(FetchMode.JOIN)
     private Category category;
 
     public FakeStoreProductDto getFakeStoreProductDto() {
